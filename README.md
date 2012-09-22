02c4ade33770f49e6b3eea569b8505fa83e5bbc9
sarroutbi <sarroutbi@parsemad.(none)>
2012-09-22 12:39:57 +0200

    Changes to get contents from mammoth

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 buscobici
 =========
+This projects allows to download information
+for the bycicles web places indicated.
--- getscripts/mammoth_get.sh
+++ getscripts/mammoth_get.sh
@@ -2,4 +2,20 @@
 #
 # This script gets all the bicycles from
 # Mammoth store !
-# URL
+
+OUTPUT_FILE=bicimania.txt
+URL="www.mammoth.es"
+URL_BASE=http://www.mammoth.es/catalogo/bicicletas?page=
+EXCLUDE="-Rgif -Rpng -Rjpg -Rcss"
+#EXCLUDE="-Ahtml"
+#ONLY_DOMAIN="bicimania.com"
+#HOST_ONLY="-D${ONLY_DOMAIN}"
+
+pages="$(seq 1 100)"
+for page in ${pages}
+do
+  echo
+  echo "wget ${EXCLUDE} ${HOST_ONLY} ${URL_BASE}${page}"
+  echo
+  wget ${EXCLUDE} ${HOST_ONLY} ${URL_BASE}${page}
+done
