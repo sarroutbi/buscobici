dc8bca68e52a67d7d1187733757e7f41cf71a532
sarroutbi <marsemad>
2012-09-24 12:43:47 +0200

    Adding a TODOs directory. Inserting more info to webtodb.sh script

--- webtodb.sh
+++ webtodb.sh
@@ -2,13 +2,23 @@
 #
 # This script receives:
 #
-# 1 - The configuration file containing the entries
+# 1 - The configuration file containing the entries.
 #     It must contain entries like:
 #     ...
 #     [7.6 FX WSD]
 #     SUBURL="http://www.mammoth.es/producto/bicicletas/trek/76-fx-wsd"
 #     TRADEMARK=Trek
-#     PRICE=1099,00€
+#     PRICE=1099,00
 #     STORE=Mammoth 
+#     KIND=MTB
 #
 #     The script will attempt to get this entries, and insert them on 
+#     the databases, in sentences like:
+#
+#     INSERT INTO bikes VALUES ('7.6 FX WSD', 'Trek', 'Mammoth', 'http://www.mammoth.es/producto/bicicletas/trek/76-fx-wsd', 'MTB', '1099.00');
+#
+
+DATABASE=bikesearch
+TABLE=bikes
+QUERY_STR="INSERT INTO bikes VALUES ('%s', '%s', '%s', '%s', '%s', '%s');
+
