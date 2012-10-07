b6083a577a074ed27d45d7683a19c5e1ad902243
sarroutbi <marsemad>
2012-10-07 13:53:33 +0200

    Adding correct html values

--- web/search.php
+++ web/search.php
@@ -26,7 +26,7 @@ if (   ((!$search)    || (strlen($search)==0))
     && ((!$priceFrom) || (strlen($priceFrom)==0)) 
     && ((!$priceTo)   || (strlen($priceTo)==0))   ) {
   echo "<section id=\"search_error\">\n";
-  echo "<p>Empty search</p>\n";
+  echo "<p>B&uacute;squeda vac&iacute;a</p>\n";
   echo "</section>\n";
   echo "</html>\n";
   exit();
@@ -34,7 +34,7 @@ if (   ((!$search)    || (strlen($search)==0))
 
 if (($priceFrom >= $priceTo) && ($priceFrom) && ($priceTo)) {
   echo "<section id=\"search_error\">\n";
-  echo "<p>Búsqueda errónea. Por favor, especfique un rango de precios válidos</p>\n";
+  echo "<p>B&uacute;squeda errónea. Por favor, especfique un rango de precios v&aacute;lidos.</p>\n";
   echo "</section>\n";
   echo "</html>\n";
   exit();
