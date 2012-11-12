e5da284eb9ff6cdbf5a33687e387ac64f9813d0e
sarroutbi <sarroutbi@gmail.com>
2012-11-12 10:34:02 +0100

    Some lines on test checkallpsql.sh script

--- test/checkallpsql.sh
+++ test/checkallpsql.sh
@@ -14,8 +14,10 @@ test -z "${2}" || PREFIX="${2}"
 
 test -d "${DIR}" || error "Dir ${DIR} does not exist" 1
 
+echo "======================================="
 for file in $(ls ${DIR}/${PREFIX}*); 
 do
   echo -n "File:$file"
   ./checkpsql.sh $file
 done
+echo "======================================="
