912e207502fc5d98e1cbac729422e68eceaba5ef
sarroutbi <sarroutbi@gmail.com>
2012-11-13 16:09:23 +0100

    Adding parsing of each of the sql fields

--- test/checkpsql.sh
+++ test/checkpsql.sh
@@ -3,7 +3,9 @@
 # This script checks some basic features for the loadable psql files, such as:
 # - The insert lines starts with "INSERT INTO"
 # - The insert lines ends with ");"
-#
+# - Check price has correct type
+# - Check URL has correct syntax (http://...)
+# - Check Model contains no forbidden characters
 # The script receives the file generated with command "webtodb.sh"
 #
 
@@ -34,6 +36,26 @@ do
   let COUNTER_ERRORS=${COUNTER_ERRORS}+1
   let ERR=${ERR}+1
 done < ${TMP_FILE} >> ${OUTPUT_FILE}
+
+cat "${1}" > ${TMP_FILE}
+
+while read line;
+do
+  MODEL=$(echo "${line}" | grep -o "(.*)" | tr -d "()" | awk -F "," {'print $1'})
+  TRADEMARK=$(echo "${line}" | grep -o "(.*)" | tr -d "()" | awk -F "," {'print $2'} | sed -e 's/^[ \t]//g')
+  STORE=$(echo "${line}" | grep -o "(.*)" | tr -d "()" | awk -F "," {'print $3'} | sed -e 's/^[ \t]//g')
+  URL=$(echo "${line}" | grep -o "(.*)" | tr -d "()" | awk -F "," {'print $4'} | sed -e 's/^[ \t]//g')
+  TYPE=$(echo "${line}" | grep -o "(.*)" | tr -d "()" | awk -F "," {'print $5'} | sed -e 's/^[ \t]//g')
+  PRICE=$(echo "${line}" | grep -o "(.*)" | tr -d "()" | awk -F "," {'print $6'} | sed -e 's/^[ \t]//g')
+  #echo "========================"
+  #echo "MODEL=${MODEL}"
+  #echo "TRADEMARK=${TRADEMARK}"
+  #echo "STORE=${STORE}"
+  #echo "URL=${URL}"
+  #echo "TYPE=${TYPE}"
+  #echo "PRICE=${PRICE}"
+  #echo "========================"
+done < ${TMP_FILE}
 
 printf "\nTotal errors : %d/%d\n" "${COUNTER_ERRORS}" "${COUNTER_LINES}"
 
