ff174f280997a3cae2c7e1ab773fb816d8d5b123
sarroutbi <sarroutbi@gmail.com>
2013-01-15 17:26:11 +0100

    Adding checker of statistics of models by price

---
+++ stats/check_stats_models_type.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+while read line; 
+do 
+  echo
+  echo "STORE:$(echo $line | awk -F "," {'print $1'})"; 
+  echo -n "\`- Columns add:"; echo "$(echo -n $line | awk -F "," {'for (i=2; i<NF; ++i){printf $i; if (i<NF-1){printf "+"}'}})" | bc; 
+  echo -n "\`- Total in DB:"; echo "$(echo $line | awk -F "," {'print $7'})" | tr -d ' '; 
+done<stats_models_type.txt
+echo
