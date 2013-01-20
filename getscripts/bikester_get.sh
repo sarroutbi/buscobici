4ee63135158ba73275c048f3bbbbb45af96bc32c
sarroutbi <sarroutbi@gmail.com>
2013-01-20 10:32:27 +0100

    Adding new Bikester get script

---
+++ getscripts/bikester_get.sh
@@ -0,0 +1,99 @@
+#!/bin/bash
+#
+# Copyright © 2012-2013 Sergio Arroutbi Braojos <sarroutbi@gmail.com>
+# 
+# Permission to use, copy, modify, and/or distribute this software 
+# for any purpose with or without fee is hereby granted, provided that 
+# the above copyright notice and this permission notice appear in all copies.
+# 
+# THE SOFTWARE IS PROVIDED “AS IS” AND THE AUTHOR DISCLAIMS ALL WARRANTIES 
+# WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY 
+# AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT, 
+# INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM 
+# LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, 
+# NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE 
+# OR PERFORMANCE OF THIS SOFTWARE.
+#
+# This script gets all the bicycles from
+# Biciescapa store !
+#
+URL="www.bikester.es"
+ONLY_DOMAIN="bikester.es"
+MAX_TRIES=10
+MAX_TIMEOUT=10
+
+function get_page()
+{
+  BASE_URL="$1"
+  PAGES="$2"
+  if [ "${PAGES}" = "" ];
+  then
+    wget --tries=${MAX_TRIES} --timeout=${MAX_TIMEOUT} "${BASE_URL}" 
+  else
+    for page in ${PAGES};
+    do
+      wget --tries=${MAX_TRIES} --timeout=${MAX_TIMEOUT} "${BASE_URL}${page}" 
+    done 
+  fi
+}
+
+MTB_FIX_BIKES_BASE="${URL}/bicicletas/bicicletas-de-montana.html?page="
+MTB_FIX_BIKES_PAGES="$(seq 0 5)"
+
+MTB_DOUBLE_BIKES_BASE="${URL}/bicicletas/bicicletas-doble-suspension.html?page="
+MTB_DOUBLE_BIKES_PAGES="$(seq 0 5)"
+
+MTB_29_BIKES_BASE="${URL}/bicicletas/bicicletas-29-pulgadas.html?page="
+MTB_29_BIKES_PAGES="$(seq 0 3)"
+
+KIDS_BIKES_BASE="${URL}/bicicletas/bicicletas-para-ninos.html?page="
+KIDS_BIKES_PAGES="$(seq 0 7)"
+
+BTT_KIDS_BIKES_BASE="${URL}/bicicletas/bicicletas-juveniles-bicicletas-todo-terreno.html?page="
+BTT_KIDS_BIKES_PAGES="$(seq 0 1)"
+
+BMX_BIKES_BASE="${URL}/bicicletas/bmx.html?page="
+BMX_BIKES_PAGES="$(seq 0 5)"
+
+ROAD_BIKES_BASE="${URL}/bicicletas/bicicletas-de-carretera.html?page="
+ROAD_BIKES_PAGES="$(seq 0 5)"
+
+ROAD_CICLOCROSS_BIKES_BASE="${URL}/bicicletas/bicicletas-fitness-bicicletas-ciclocross.html"
+ROAD_CICLOCROSS_BIKES_PAGES=""
+
+URBAN_XC_BIKES_BASE="${URL}/bicicletas/bicicletas-xc-.html?page="
+URBAN_XC_BIKES_PAGES="$(seq 0 3)"
+
+URBAN_SINGLE_BIKES_BASE="${URL}/bicicletas/bicicletas-single-speed.html"
+URBAN_SINGLE_BIKES_PAGES=""
+
+URBAN_WALK_BIKES_BASE="${URL}/bicicletas/bicicleta-de-paseo.html?page="
+URBAN_WALK_BIKES_PAGES="$(seq 0 2)"
+
+URBAN_FOLDING_BIKES_BASE="${URL}/bicicletas/bicicletas-plegables-.html?page="
+URBAN_FOLDING_BIKES_PAGES="$(seq 0 2)"
+
+URBAN_RETRO_BIKES_BASE="${URL}/bicicletas/cruiser-retro.html"
+URBAN_RETRO_BIKES_PAGES=""
+
+URBAN_ELECT_BIKES_BASE="${URL}/bicicletas/bicicletas-electricas-pedelec.html?page="
+URBAN_ELECT_BIKES_PAGES="$(seq 0 2)"
+
+TREKKING_BIKES_BASE="${URL}/bicicletas/bicicletas-trekking.html?page="
+TREKKING_BIKES_PAGES="$(seq 0 3)"
+
+get_page "${MTB_FIX_BIKES_BASE}"       "${MTB_FIX_BIKES_PAGES}"   
+get_page "${MTB_DOUBLE_BIKES_BASE}"    "${MTB_DOUBLE_BIKES_PAGES}"   
+get_page "${MTB_29_BIKES_BASE}"        "${MTB_29_BIKES_PAGES}"   
+get_page "${KIDS_BIKES_BASE}"          "${KIDS_BIKES_PAGES}"   
+get_page "${BTT_KIDS_BIKES_BASE}"      "${BTT_KIDS_BIKES_PAGES}"   
+get_page "${BMX_KIDS_BIKES_BASE}"      "${BMX_KIDS_BIKES_PAGES}"   
+get_page "${ROAD_BIKES_BASE}"          "${ROAD_BIKES_PAGES}"   
+get_page "${ROAD_CICLOCROSS_BASE}"     "${ROAD_CICLOCROSS_PAGES}"   
+get_page "${URBAN_XC_BIKES_BASE}"      "${URBAN_SC_BIKES_PAGES}"   
+get_page "${URBAN_SINGLE_BIKES_BASE}"  "${URBAN_SINGLE_BIKES_PAGES}"   
+get_page "${URBAN_WALK_BIKES_BASE}"    "${URBAN_WALK_BIKES_PAGES}"   
+get_page "${URBAN_FOLDING_BIKES_BASE}" "${URBAN_FOLDING_BIKES_PAGES}"   
+get_page "${URBAN_RETRO_BIKES_BASE}"   "${URBAN_RETRO_BIKES_PAGES}"   
+get_page "${URBAN_ELECT_BIKES_BASE}"   "${URBAN_ELECT_BIKES_PAGES}"   
+get_page "${TREKKING_BIKES_BASE}"      "${TREKKING_BIKES_PAGES}"   
