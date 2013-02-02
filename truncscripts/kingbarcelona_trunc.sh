2b260517742b86ae64da203c09f9c9ac27788639
sarroutbi <sarroutbi@gmail.com>
2013-02-02 23:46:55 +0100

    correct syntax in King Barcelona trunc script

--- truncscripts/kingbarcelona_trunc.sh
+++ truncscripts/kingbarcelona_trunc.sh
@@ -16,4 +16,4 @@
 #
 # Remember: This can be done becahse a .pgpass must exist on 
 # HOME dir, with permission 0600, containing the password
-psql --user postgres -h 192.168.0.40 bikesearch -c "DELETE FROM bikes WHERE store LIKE 'Kinb Barcelona';"
+psql --user postgres -h 192.168.0.40 bikesearch -c "DELETE FROM bikes WHERE store LIKE 'King Barcelona';"
