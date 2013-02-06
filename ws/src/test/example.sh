de3d14cfd673e30ab0c4ace7dcb5c998f4cf140d
sarroutbi <sarroutbi@gmail.com>
2013-02-06 16:41:37 +0100

    Adding Web Service directory, to implement a CURL based Web Service

---
+++ ws/src/test/example.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+./test_post "http://buscobici.com/bikesearch/search.php" "search=trek&priceFrom=500&priceTo=550&type=MTB"
---
+++ ws/src/test/test_post.cpp
@@ -0,0 +1,66 @@
+#include <stdio.h>
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+#include <curl/curl.h>
+#ifdef __cplusplus
+}
+#endif
+
+int parse_response(CURL *curl)
+{
+  fprintf(stderr, "========================== Performing analysis ");
+  fprintf(stderr, " ==========================\n");
+  fprintf(stderr, "===============================================");
+  fprintf(stderr, "===========================\n");
+}
+
+int main(int argc, char* argv[])
+{
+  CURL *curl;
+  CURLcode ret;
+
+  curl_global_init(CURL_GLOBAL_ALL);
+
+  if ((argc < 2) || (!argv[1]))
+  {
+    fprintf(stderr, "Please provide a URL \n");
+    goto error_url;
+  }
+  else if((argc<3) || (!argv[2]))
+  {
+    fprintf(stderr, "Please provide the data to post\n");
+    goto error_post;
+  }
+  /* get a curl handle */ 
+  curl = curl_easy_init();
+  if(curl) 
+  {
+    /* URL set */ 
+    curl_easy_setopt(curl, CURLOPT_URL, argv[1]);
+
+    /*  POST data */ 
+    curl_easy_setopt(curl, CURLOPT_POSTFIELDS, argv[2]);
+
+    /* Perform the request, ret is the return code */ 
+    ret = curl_easy_perform(curl);
+    
+    /
+    /* Check for errors */ 
+    if(ret != CURLE_OK)
+    {
+      fprintf(stderr, "curl_easy_perform() failed: %s\n", 
+              curl_easy_strerror(ret));
+    }
+    /* always cleanup */ 
+    curl_easy_cleanup(curl);
+  }
+  curl_global_cleanup();
+  return 0;
+error_url:
+  return 1;
+error_post:
+  return 2;
+}
