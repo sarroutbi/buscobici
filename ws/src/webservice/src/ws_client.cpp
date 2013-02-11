d9d5a851f02fa28cc80cb4aa3e917d6b90c53744
sarroutbi <sarroutbi@gmail.com>
2013-02-11 16:05:39 +0100

    Adding parsing of parameters to the web server client

--- ws/src/webservice/src/ws_client.cpp
+++ ws/src/webservice/src/ws_client.cpp
@@ -1,6 +1,7 @@
 #include "soapH.h"
 #include "bikeGet.nsmap"
-#include "Bike.h"
+
+const uint8_t MAX_POST_FIELD=255;
 
 void logList(const ns2BikeList & ns2bl)
 {
@@ -9,7 +10,8 @@ void logList(const ns2BikeList & ns2bl)
   for(i = bl.begin(); i != bl.end(); ++i)
   {
     std::cout << "===================================================="
-              << "====================================================" << std::endl;
+              << "====================================================" 
+              << std::endl;
     std::cout << "Trademark:" << i->trademark << std::endl;
     std::cout << "Model    :" << i->model<< std::endl;
     std::cout << "Store    :" << i->store<< std::endl;
@@ -17,16 +19,73 @@ void logList(const ns2BikeList & ns2bl)
     std::cout << "Url Text :" << i->urlContent<< std::endl;
     std::cout << "Price    :" << i->price<< std::endl;
     std::cout << "===================================================="
-              << "====================================================" << std::endl;
+              << "====================================================" 
+              << std::endl;
   }
 }
 
-int main()
+void usage(char* argv, uint8_t err)
+{
+  if(argv)
+  {
+    fprintf(stderr, "", argv);
+    fprintf(stderr, "%s: ", argv);
+    fprintf(stderr, "	%s [-s\"search\"] -k[\"bike_kind\"] -f\
+                    [\"price_from\"] -t[\"price_to\"]", argv);
+    fprintf(stderr, "", argv);
+  }
+  exit(err);
+}
+
+void parseArgs(int argc, char* argv[], char* search, uint8_t search_max,
+               char* type, uint8_t type_max, uint32_t* priceFrom, 
+               uint32_t* priceTo)
+{
+  int8_t  c = 0;
+  uint8_t opterr = 0;
+     
+  while ((c = getopt (argc, argv, "s:k:f:t:")) != -1)
+  {
+    switch (c)
+    {
+      case 's':
+        strncpy(search, optarg, search_max);
+        break;
+      case 'k':
+        strncpy(type, optarg, type_max);
+        break;
+      case 'f':
+        *priceFrom = strtoul(optarg, 0, 10);
+        break;
+      case 't':
+        *priceTo = strtoul(optarg, 0, 10);
+        break;
+      case '?':
+        if (optopt == 'c')
+          fprintf (stderr, "Option -%c requires an argument.\n", optopt);
+        else 
+          fprintf (stderr, "Unknown option `-%c'.\n", optopt);
+      default:
+        usage (argv[0], 1);
+    }
+  }
+  return;
+}
+
+int main(int argc, char* argv[])
 {
   ns2BikeList bl;
   struct soap *soap = soap_new();
-  if(soap_call_ns2__bikeGet(soap, NULL, NULL, const_cast<char*>("Trek"), 
-                            const_cast<char*>("MTB"), 0, 1000, bl) == SOAP_OK)
+  char search[MAX_POST_FIELD] = "";
+  char type  [MAX_POST_FIELD] = "";
+  uint32_t priceFrom = 0;
+  uint32_t priceTo   = 0;
+ 
+  parseArgs(argc, argv, search, MAX_POST_FIELD, type, MAX_POST_FIELD, 
+            &priceFrom, &priceTo);
+
+  if(soap_call_ns2__bikeGet(soap, NULL, NULL, search, type, priceFrom, 
+                            priceTo, bl) == SOAP_OK)
   {
     logList(bl);
   }
