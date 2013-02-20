f81e0f2ab06ba2ab99918e3fbb3e881988dfd57f
sarroutbi <sarroutbi@gmail.com>
2013-02-20 10:49:56 +0100

    Fixing errors related to type parsing in complex search

--- web/search.php
+++ web/search.php
@@ -1,4 +1,4 @@
-<?php
+<?php 
 /**
  * Copyright © 2012-2013 Sergio Arroutbi Braojos <sarroutbi@gmail.com>
  * 
@@ -43,13 +43,46 @@ echo "<title>Buscador de Bicicletas - Comprar Bici - Buscar</title>\n";
 echo "<link rel=\"stylesheet\" href=\"bikesearch.css\" type=\"text/css\">\n";
 echo "</head>\n";
 
+echo "<body>\n";
+
+// echo "<p>=============== POST =================</p>\n";
+// echo "<p>search:=>$search<=</p>";
+// echo "<p>PriceFrom:=>$priceFrom<=</p>";
+// echo "<p>PriceTo:=>$priceTo<=</p>";
+// echo "<p>Type:=>$type<=</p>";
+// echo "<p>QUERY:=>$query<=</p>";
+// echo "<p>RESULT:=>$result<=</p>";
+// echo "<p>=============== POST ==================</p>\n";
+// echo "<p>=============== GET  =================</p>";
+// echo "<p>searchGet:=>$searchGet<=</p>";
+// echo "<p>PriceFrom:=>$priceFromGet<=</p>";
+// echo "<p>PriceTo:=>$priceToGet<=</p>";
+// echo "<p>Type:=>$typeGet<=</p>";
+// echo "<p>QUERY:=>$queryGet<=</p>";
+// echo "<p>RESULT:=>$result<=</p>";
+// echo "<p>=============== GET ==================</p>\n";
+// echo "<section id=\"search_results\">\n";
+// echo "<section id=\"search_results\">\n";
+
+
 if ( !$search) {
   $search=$searchGet;
+}
+if ( !$priceFrom) {
+  $priceFrom=$priceFromGet;
+}
+if ( !$priceTo) {
+  $priceTo=$priceToGet;
+}
+if ( !$type) {
+  $type=$typeGet;
 }
 
 if (   ((!$search)    || (strlen($search)==0))
     && ((!$priceFrom) || (strlen($priceFrom)==0)) 
-    && ((!$priceTo)   || (strlen($priceTo)==0))   ) {
+    && ((!$priceTo)   || (strlen($priceTo)==0))   
+    && ((!$type)      || (strlen($type)==0))   
+    ) {
   echo "<section id=\"search_error\">\n";
   echo "<p>B&uacute;squeda vac&iacute;a</p>\n";
   echo "</section>\n";
@@ -99,29 +132,25 @@ if (($priceFrom < $priceTo)) {
   }
 }
 else {
-  $query .= "ORDER BY price;";
+  if($search) {
+    if($type) {
+      $query .= "AND kind ~ '$type' ORDER BY price;";
+    }
+    else {
+      $query .= "ORDER BY price;";
+    }
+  } 
+  else {
+    if($type) {
+      $query .= "WHERE kind ~ '$type' ORDER BY price;";
+    }
+    else {
+      $query .= "ORDER BY price;";
+    }
+  }
 }
 
 $result = pg_query($query) or die('Query returned an error: ' . pg_last_error());
-
-#echo "<p>=============== POST =================</p>\n";
-#echo "<p>search:=>$search<=</p>\n";
-#echo "<p>PriceFrom:=>$priceFrom<=</p>\n";
-#echo "<p>PriceTo:=>$priceTo<=</p>\n";
-#echo "<p>Type:=>$type<=</p>\n";
-#echo "<p>QUERY:=>$query<=</p>\n";
-#echo "<p>RESULT:=>$result<=</p>\n";
-#echo "<p>=============== POST ==================</p>\n";
-#echo "<p>=============== GET  =================</p>\n";
-#echo "<p>searchGet:=>$searchGet<=</p>\n";
-#echo "<p>PriceFrom:=>$priceFromGet<=</p>\n";
-#echo "<p>PriceTo:=>$priceToGet<=</p>\n";
-#echo "<p>Type:=>$typeGet<=</p>\n";
-#echo "<p>QUERY:=>$queryGet<=</p>\n";
-#echo "<p>RESULT:=>$result<=</p>\n";
-#echo "<p>=============== GET ==================</p>\n";
-// echo "<section id=\"search_results\">\n";
-// echo "<section id=\"search_results\">\n";
 
 echo "<section>\n";
         echo "<nav>\n";
@@ -191,20 +220,20 @@ if ($round==0) {
           echo "<section id=\"search_error\">\n";
           echo "<p>Sin resultados</p>\n";
           echo "</section>\n";
-          echo "</html>\n";
-          exit();
 }
 if ($num_results!=0) {
           echo "<section id=\"num_results\">\n";
           echo "<p>N&uacute;mero de resultados:$num_results</p>\n";
           echo "</section>\n";
-          echo "</html>\n";
-          exit();
 }
+echo "</body>\n";
+echo "</html>\n";
 
 // Freeing result
 pg_free_result($result);
 
 // Closing connection
 pg_close($dbconn);
+
+exit();
 ?>
