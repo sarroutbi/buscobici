17c2e04d5c07e9ead4ccf39125798f7524d97712
sarroutbi <sarroutbi@gmail.com>
2013-02-27 14:01:44 +0100

    Adding new web pager to make searchs easier

--- web/search.php
+++ web/search.php
@@ -23,14 +23,50 @@ $dbconn = pg_connect("host=localhost dbname=bikesearch user=postgres password=po
     or die('Could not connect: ' . pg_last_error());
 
 // SQL Query
-$search    = $_POST['search'];
-$priceFrom = $_POST['priceFrom'];
-$priceTo   = $_POST['priceTo'];
-$type      = $_POST['type'];
-$searchGet    = $_GET['search'];
-$priceFromGet = $_GET['priceFrom'];
-$priceToGet   = $_GET['priceTo'];
-$typeGet      = $_GET['type'];
+$search          = $_POST['search'];
+$priceFrom       = $_POST['priceFrom'];
+$priceTo         = $_POST['priceTo'];
+$type            = $_POST['type'];
+$page            = $_POST['page'];
+$numResults      = $_POST['numResults'];
+$entryPerPage    = $_POST['entryPerPage'];
+$searchGet       = $_GET['search'];
+$priceFromGet    = $_GET['priceFrom'];
+$priceToGet      = $_GET['priceTo'];
+$typeGet         = $_GET['type'];
+$pageGet         = $_GET['page'];
+$numResultsGet   = $_GET['numResults'];
+$entryPerPageGet = $_GET['entryPerPage'];
+
+// Default Num. of results per page
+$defaultResultsPerPage = 15;
+$defaultAroundPages    = 2;
+$defaultFirstPages     = 5;
+$defaultLastPages      = 5;
+$defaultMaxModel       = 25;
+
+if ( ($numResultsGet     && ($numResultsGet   != "" )) ||
+     ($numResults        && ($numResults      != "" )) ||
+     ($entryPerPage      && ($entryPerPage    != "" )) || 
+     ($entryPerPageGet   && ($entryPerPageGet != "" )) )  {
+  if ($numResults && ($numResults != ""))
+    $resultsPerPage = $numResults;
+  else if ($numResultsGet && ($numResultsGet != "")) 
+    $resultsPerPage = $numResultsGet;
+  else if ($entryPerPage && ($entryPerPage != ""))
+    $resultsPerPage = $entryPerPage;
+  else if ($entryPerPageGet && ($entryPerPageGet != ""))
+    $resultsPerPage = $entryPerPageGet;
+}
+else {
+  $resultsPerPage = $defaultResultsPerPage;
+}
+
+// Actual page
+if($pageGet  && ($pageGet != ""))
+  $page = $pageGet;
+else if(!$page  || ($page == ""))
+  $page = 1;
 
 // HTML Results Print
 
@@ -50,20 +86,19 @@ echo "<body>\n";
 // echo "<p>PriceFrom:=>$priceFrom<=</p>";
 // echo "<p>PriceTo:=>$priceTo<=</p>";
 // echo "<p>Type:=>$type<=</p>";
-// echo "<p>QUERY:=>$query<=</p>";
-// echo "<p>RESULT:=>$result<=</p>";
+// echo "<p>Entries per page:=>$entryPerPage<=</p>";
+// echo "<p>Results per page:=>$resultsPerPage<=</p>";
 // echo "<p>=============== POST ==================</p>\n";
 // echo "<p>=============== GET  =================</p>";
 // echo "<p>searchGet:=>$searchGet<=</p>";
 // echo "<p>PriceFrom:=>$priceFromGet<=</p>";
 // echo "<p>PriceTo:=>$priceToGet<=</p>";
 // echo "<p>Type:=>$typeGet<=</p>";
-// echo "<p>QUERY:=>$queryGet<=</p>";
-// echo "<p>RESULT:=>$result<=</p>";
+// echo "<p>Entries per page:=>$entryPerPageGet<=</p>";
+// echo "<p>Results per page:=>$resultsPerPage<=</p>";
 // echo "<p>=============== GET ==================</p>\n";
 // echo "<section id=\"search_results\">\n";
 // echo "<section id=\"search_results\">\n";
-
 
 if ( !$search) {
   $search=$searchGet;
@@ -105,6 +140,7 @@ if (($priceFrom >= $priceTo) && ($priceFrom) && ($priceTo)) {
 }
 
 $search_array=str_word_count($search, 1, '0123456789-/.');
+$start = ($page - 1) * $resultsPerPage;
 $query = "SELECT trademark, model, kind, CAST(price AS decimal(10,2)), store, url FROM bikes ";
 $round = 0;
 $num_results = 0;
@@ -122,39 +158,53 @@ foreach ($search_array as $subsearch) {
 if (($priceFrom < $priceTo)) {
   if($search) {
     if($type) {
-      $query .= "AND (price <= '$priceTo' AND price >= '$priceFrom') AND kind ~ '$type' ORDER BY price;";
+      $query .= "AND (price <= '$priceTo' AND price >= '$priceFrom') AND kind ~ '$type' ORDER BY price";
     }
     else {
-      $query .= "AND (price <= '$priceTo' AND price >= '$priceFrom') ORDER BY price;";
+      $query .= "AND (price <= '$priceTo' AND price >= '$priceFrom') ORDER BY price";
     }
   }
   else {
     if($type) {
-      $query .= "WHERE (price <= '$priceTo' AND price >= '$priceFrom') AND kind ~ '$type' ORDER BY price;";
+      $query .= "WHERE (price <= '$priceTo' AND price >= '$priceFrom') AND kind ~ '$type' ORDER BY price";
     }
     else {
-      $query .= "WHERE (price <= '$priceTo' AND price >= '$priceFrom') ORDER BY price;";
+      $query .= "WHERE (price <= '$priceTo' AND price >= '$priceFrom') ORDER BY price";
     }
   }
 }
 else {
   if($search) {
     if($type) {
-      $query .= "AND kind ~ '$type' ORDER BY price;";
+      $query .= "AND kind ~ '$type' ORDER BY price";
     }
     else {
-      $query .= "ORDER BY price;";
+      $query .= "ORDER BY price";
     }
   } 
   else {
     if($type) {
-      $query .= "WHERE kind ~ '$type' ORDER BY price;";
+      $query .= "WHERE kind ~ '$type' ORDER BY price";
     }
     else {
-      $query .= "ORDER BY price;";
+      $query .= "ORDER BY price";
     }
   }
 }
+
+$query_limited = $query . " LIMIT $resultsPerPage OFFSET $start;";
+$query .= ";";
+
+// echo "<p>=============== POST =================</p>\n";
+// echo "<p>QUERY:=>$query<=</p>";
+// echo "<p>QUERY LIMITED:=>$query_limited<=</p>";
+// echo "<p>RESULT:=>$result<=</p>";
+// echo "<p>=============== POST ==================</p>\n";
+// echo "<p>=============== GET  =================</p>";
+// echo "<p>QUERY:=>$queryGet<=</p>";
+// echo "<p>QUERY LIMITED:=>$query_limited<=</p>";
+// echo "<p>RESULT:=>$result<=</p>";
+// echo "<p>=============== GET ==================</p>\n";
 
 $result = pg_query($query) or die('Query returned an error: ' . pg_last_error());
 
@@ -171,12 +221,15 @@ echo "<section>\n";
         echo "<br>\n";
         echo "<br>\n";
 
-echo "<table class=\"tableSearch\">\n";
-
 $round = 0;
 
+$num_results = pg_numrows($result);
+$num_pages = ceil($num_results / $resultsPerPage);
+$result = pg_query($query_limited) or die('Query returned an error: ' . pg_last_error());
+
+echo "<table class=\"tableSearch\">\n";
+
 while ($line = pg_fetch_array($result, null, PGSQL_ASSOC)) {
-    $num_results += 1;
     if ($round== 0)
     {
         echo "\t<thead>\n";
@@ -202,10 +255,15 @@ while ($line = pg_fetch_array($result, null, PGSQL_ASSOC)) {
         }
         if($round == 1) {
           $model = $col_value;
+          $limited_model = $model;
+          if (strlen($model) > $defaultMaxModel) {
+            $limited_model = substr($model, 0, $defaultMaxModel-3);
+            $limited_model .= "...";
+            $col_value = $limited_model;
+          }
         }
-
         if ( strcmp($islink, $http_line) == 0 ) {
-          echo "\t\t<td><a href=\"$col_value\" target=\"_blank\">$trademark_trim - $model</a></td>\n";
+          echo "\t\t<td><a href=\"$col_value\" target=\"_blank\">$trademark_trim - $limited_model</a></td>\n";
         }
         else {
           if ( strcmp($col_value, "URBAN-CONFORT-FOLDING") == 0 ) {
@@ -215,7 +273,7 @@ while ($line = pg_fetch_array($result, null, PGSQL_ASSOC)) {
             echo "\t\t<td>$col_value</td>\n";
           }
         }
-	$round = $round+1;
+      $round = $round+1;
     }
     echo "\t</tr>\n";
 }
@@ -223,15 +281,96 @@ echo "\t</tbody>\n";
 echo "</table>\n";    
 echo "</section>\n";
 if ($round==0) {
-          echo "<section id=\"search_error\">\n";
-          echo "<p>Sin resultados</p>\n";
-          echo "</section>\n";
+    echo "<section id=\"search_error\">\n";
+    echo "<p>Sin resultados</p>\n";
+    echo "</section>\n";
 }
-if ($num_results!=0) {
-          echo "<section id=\"num_results\">\n";
-          echo "<p>N&uacute;mero de resultados:$num_results</p>\n";
-          echo "</section>\n";
+
+if ($num_results > 0) {
+    echo "<section id=\"page_results\">\n";
+    echo "<form method=\"post\" action=\"search.php?page=1&type=$type&priceFrom=$priceFrom&priceTo=$priceTo&search=$search&entryPerPage=$resultsPerPage\" name=\"form\">";
+
+    echo "<p>N&uacute;mero total de resultados:$num_results";
+    if ($num_results!=0) {
+      if (($num_pages) > 1) {
+        echo " - Mostrando p&aacute;gina:$page/$num_pages - ";
+        $plotsMinPainted = false;
+        $plotsMaxPainted = false;
+        $pending_page = 1;
+        if ($page != 1) {
+          $prev_page = $page-1;
+          echo "<a href=\"search.php?page=$prev_page&type=$type&priceFrom=$priceFrom&priceTo=$priceTo&search=$search&entryPerPage=$resultsPerPage\">&lt;&lt;&lt;</a>\n";
+          echo " - ";
+        }
+        while ( $pending_page <= $num_pages) {
+          if( ($pending_page <= ($page + $defaultAroundPages)) && ($pending_page >= ($page - $defaultAroundPages)) ||
+              ($pending_page <= $defaultFirstPages) || ($pending_page > ($num_pages - $defaultLastPages)) ) {
+            if ($page == $pending_page) {
+              echo "<b>$pending_page</b>\n";
+            }
+            else {
+              echo "<a href=\"search.php?page=$pending_page&type=$type&priceFrom=$priceFrom&priceTo=$priceTo&search=$search&entryPerPage=$resultsPerPage\">$pending_page</a>\n";
+            }
+          }
+          else {
+            if(($pending_page > $defaultFirstPages) && ($pending_page < ($page - $defaultAroundPages)) ) {
+              if (!$plotsMinPainted) {
+                echo " ... ";
+                $plotsMinPainted = true;
+              }
+            }
+            if($pending_page < ($num_pages - $defaultLastPages) && ($pending_page > ($page + $defaultAroundPages)) ) { 
+              if (!$plotsMaxPainted) {
+                echo " ... ";
+                $plotsMaxPainted = true;
+              }
+            }
+          }
+          $pending_page++;
+        }
+        if ($page != $num_pages) {
+          $next_page = $page+1;
+          echo " - ";
+          echo "<a href=\"search.php?page=$next_page&type=$type&priceFrom=$priceFrom&priceTo=$priceTo&search=$search&entryPerPage=$resultsPerPage\">&gt;&gt;&gt;</a>\n";
+          echo " ";
+        }
+        if (($num_pages) > 1) {
+          echo "<label> - Entradas por p&aacute;gina :</label>\n";
+          echo "<select name=\"entryPerPage\" id=\"entryPerPage\" onSelect=\"form.submit()\" onChange=\"form.submit()\">\n";
+          if ($resultsPerPage == 10) 
+            echo "  <option value=\"10\" selected=\"selected\">10</option>\n";
+	  else 
+            echo "  <option value=\"10\">10</option>\n";
+          if ($resultsPerPage == 15) 
+            echo "  <option value=\"15\" selected=\"selected\">15</option>\n";
+	  else 
+            echo "  <option value=\"15\">15</option>\n";
+          if ($resultsPerPage == 20) 
+            echo "  <option value=\"20\" selected=\"selected\">20</option>\n";
+	  else 
+            echo "  <option value=\"20\">20</option>\n";
+          if ($resultsPerPage == 25) 
+            echo "  <option value=\"25\" selected=\"selected\">25</option>\n";
+          else
+            echo "  <option value=\"25\">25</option>\n";
+          if ($resultsPerPage == 50)
+            echo "  <option value=\"50\" selected=\"selected\">50</option>\n";
+          else
+            echo "  <option value=\"50\">50</option>\n";
+          if ($resultsPerPage == 100)
+            echo "  <option value=\"100\" selected=\"selected\">100</option>\n";
+          else
+            echo "  <option value=\"100\">100</option>\n";
+          echo "</select>\n";
+        }
+      }
+      echo "</p>";
+      echo "</section>\n";
+      // echo "<input id =\"submit2\" name=\"submit\" type=\"submit\" value=\"Actualizar\">\n";
+      echo "</form>\n";
+    }
 }
+
 echo "</body>\n";
 echo "</html>\n";
 
