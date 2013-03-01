bdf10ff534937a18a97a15fe918bf154f248d38d
sarroutbi <sarroutbi@gmail.com>
2013-03-01 22:04:00 +0100

    Adding tracking to search page

--- web/search.php
+++ web/search.php
@@ -77,6 +77,18 @@ echo "<head>\n";
 echo "<meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\">\n";
 echo "<title>Buscador de Bicicletas - Comprar Bici - Buscar</title>\n";
 echo "<link rel=\"stylesheet\" href=\"bikesearch.css\" type=\"text/css\">\n";
+echo "<script type=\"text/javascript\">\n";
+echo "  var _gaq = _gaq || [];\n";
+echo "  _gaq.push(['_setAccount', 'UA-35382693-4']);\n";
+echo "  _gaq.push(['_trackPageview']);\n";
+echo "\n";
+echo "  (function() {\n";
+echo "    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;\n";
+echo "    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';\n";
+echo "    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);\n";
+echo "  })();\n";
+echo "</script>\n";
+
 echo "</head>\n";
 
 echo "<body>\n";
