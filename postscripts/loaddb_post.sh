d24db586172e43b5bf11941cf9afa90ba17494d2
sarroutbi <sarroutbi@gmail.com>
2013-03-02 12:59:03 +0100

    Do not allow prices under 20 euros

--- postscripts/loaddb_post.sh
+++ postscripts/loaddb_post.sh
@@ -16,6 +16,6 @@
 #
 # Remember: This can be done becahse a .pgpass must exist on 
 # HOME dir, with permission 0600, containing the password
-psql --user postgres -h 192.168.0.40 bikesearch -c "DELETE FROM bikes WHERE PRICE = 0;"
+psql --user postgres -h 192.168.0.40 bikesearch -c "DELETE FROM bikes WHERE PRICE <= 20;"
 psql --user postgres -h 192.168.0.40 bikesearch -c "DELETE FROM bikes WHERE Trademark = 'Bellelli';"
 psql --user postgres -h 192.168.0.40 bikesearch -c "DELETE FROM bikes WHERE TRADEMARK ~ 'Brompton' AND PRICE < 50;"
