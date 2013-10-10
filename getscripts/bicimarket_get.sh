1bd1556d96e42f845a66e023eb8081207626fda2
sarroutbi <sarroutbi@gmail.com>
2013-10-10 08:09:44 +0200

    Adding appropriate URLs to BiciMarket store

--- getscripts/bicimarket_get.sh
+++ getscripts/bicimarket_get.sh
@@ -80,7 +80,10 @@ FIXIE_BIKES_PAGES=""
 HYBRID_BIKES_BASE="${URL}/Bicicletas-Hibrida-c10050.html"
 HYBRID_BIKES_PAGES=""
 
-JUNIOR_BIKES_BASE="${URL}/Bicicletas-Infantil-c20177.html"
+KIDS_BIKES_BASE="${URL}/Bicicletas-Infantil-c20177.html"
+KIDS_BIKES_PAGES=""
+
+JUNIOR_BIKES_BASE="${URL}/Bicicletas-Junior-c20391.html"
 JUNIOR_BIKES_PAGES=""
 
 LADY_BIKES_BASE="${URL}/Bicicletas-Lady-c20253.html"
@@ -98,6 +101,30 @@ TRIATLON_BIKES_PAGES=""
 URBAN_BIKES_BASE="${URL}/Bicicletas-Urbanas-c10030.html"
 URBAN_BIKES_PAGES=""
 
+#DOUBLE
+MTB_27_ENDURO_BASE="${URL}/Bicicletas-Enduro-AM-27-5-c20400.html"    
+MTB_27_ENDURO_PAGES=""
+
+#DOUBLE
+MTB_29_ENDURO_BASE="${URL}/Bicicletas-Enduro-AM-29-c20404.html"
+MTB_29_ENDURO_PAGES=""
+
+#FIX
+MTB_XC_26_BASE="${URL}/Bicicletas-MTB-XC-26-c1010.html"
+MTB_XC_26_PAGES=""
+
+#DOUBLE
+MTB_XC_29_DOUBLE_BASE="${URL}/Bicicletas-MTB-XC-Doble-29-c20403.html"
+MTB_XC_29_DOUBLE_PAGES=""
+
+#FIX
+MTB_XC_27_5_FIX_BASE="${URL}/Bicicletas-MTB-XC-Rigida-27-5-c20402.html"
+MTB_XC_27_5_FIX_PAGES=""
+
+#MTB-29
+MTB_XC_29_FIX_BASE="${URL}/Bicicletas-MTB-XC-Rigida-29-c20380.html"
+MTB_XC_29_FIX_PAGES=""
+
 get_page "${MTB_29_BIKES_BASE}"
 get_page "${MTB_27_5_BIKES_BASE}"
 get_page "${ELECTRIC_BIKES_BASE}"
@@ -113,8 +140,15 @@ get_page "${MTB_DOUBLE_BIKES_BASE}"
 get_page "${FIXIE_BIKES_BASE}"
 get_page "${HYBRID_BIKES_BASE}"
 get_page "${JUNIOR_BIKES_BASE}"
+get_page "${KIDS_BIKES_BASE}"
 get_page "${LADY_BIKES_BASE}"
 get_page "${VELO_BIKES_BASE}"
 get_page "${FOLDING_BIKES_BASE}"
 get_page "${TRIATLON_BIKES_BASE}"
 get_page "${URBAN_BIKES_BASE}"
+get_page "${MTB_27_ENDURO_BASE}"
+get_page "${MTB_29_ENDURO_BASE}"
+get_page "${MTB_XC_26_BASE}"
+get_page "${MTB_XC_29_DOUBLE_BASE}"
+get_page "${MTB_XC_27_5_FIX_BASE}"
+get_page "${MTB_XC_29_FIX_BASE}"
