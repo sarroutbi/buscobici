48bcd7b6147d4e382c1fd9ef186f96716e5c8312
sarroutbi <sarroutbi@yahoo.es>
2014-04-11 19:58:33 +0200

    OTHERS: Fixing compare script for empty files cases

--- compare_psql_results.sh
+++ compare_psql_results.sh
@@ -13,31 +13,60 @@ test -d $2 || exit_error "Directory:[${2}] does not exist" 1
 ls $1/psql* >/dev/null || exit_error "Directory:[${1}] does not contain psql files" 1
 ls $2/psql* >/dev/null || exit_error "Directory:[${2}] does not contain psql files" 1
 
-for psql_file in $(ls $1/psql*); 
-do
-  DST_PSQL_FILE=$(echo $psql_file | sed s@${1}@${2}@g)
-  echo "=========================================================="
-  echo "$(basename ${psql_file}):"
-  echo -n "${1}:"
-  OLD_COUNT=$(wc -l ${psql_file} | awk {'print $1'})
-  echo ${OLD_COUNT}
-  echo -n "${2}:"
-  test -f ${DST_PSQL_FILE}
-  if [ $? -eq 0 ];
+function against() {
+  FILES=""
+  if [ "${3}" == "1" ];
   then
-    NEW_COUNT=$(wc -l $(echo $psql_file | sed s@${1}@${2}@g) | awk {'print $1'})
-    let difference=$(($NEW_COUNT - $OLD_COUNT))
-    if [ $difference -gt 0 ];
-    then
-      printf "$NEW_COUNT (+${difference})\n"
-    else 
-      printf "$NEW_COUNT (${difference})\n"
-    fi
+    for file in $(ls -1 $2/psql*);
+    do
+      bnfile=$(basename $file)
+      ls $1/${bnfile} 1>/dev/null 2>/dev/null || FILES="${FILES} ${file}" 
+    done
   else
-    echo "0"
+    FILES=$(ls -1 $1/psql*)
   fi
-  echo "=========================================================="
-done
+  for psql_file in ${FILES};
+  do
+    if [ "${3}" == "1" ]; then
+      TARGET_FILE=$(echo $psql_file | sed s@${2}@${1}@g)
+      DST_PSQL_FILE=${psql_file}
+    else
+      TARGET_FILE=${psql_file}
+      DST_PSQL_FILE=$(echo $psql_file | sed s@${1}@${2}@g)
+    fi
+    echo "=========================================================="
+    echo "$(basename ${TARGET_FILE}):"
+    echo -n "${1}:"
+    test -f ${TARGET_FILE}
+    if [ $? -eq 0 ];
+    then
+      OLD_COUNT=$(wc -l ${TARGET_FILE} | awk {'print $1'})
+      echo ${OLD_COUNT}
+    else
+      OLD_COUNT=0
+      echo "0"
+    fi
+    echo -n "${2}:"
+    test -f ${DST_PSQL_FILE}
+    if [ $? -eq 0 ];
+    then
+      NEW_COUNT=$(wc -l $(echo ${TARGET_FILE} | sed s@${1}@${2}@g) | awk {'print $1'})
+      let difference=$(($NEW_COUNT - $OLD_COUNT))
+      if [ $difference -gt 0 ];
+      then
+        printf "$NEW_COUNT (+${difference})\n"
+      else 
+        printf "$NEW_COUNT (${difference})\n"
+      fi
+    else
+      echo "0"
+    fi
+    echo "=========================================================="
+  done
+}
+
+against ${1} ${2} ""
+against ${1} ${2} "1" // Analyze only those in 2, not in 1
 echo -n "TOTAL results in [${1}]: "
 let LAST_TOTAL=$(wc -l ${1}/psql* | tail -1 | awk {'print $1'} | tr -d ' ')
 echo $LAST_TOTAL
