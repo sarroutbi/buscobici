97e47f3326a3f113b8bf4b5086f0d463d48e61e9
sarroutbi <sarroutbi@yahoo.es>
2014-08-26 15:25:16 +0200

    Adding REVIEW of MHWBike Store

--- getscripts/mhwbike_get.sh
+++ getscripts/mhwbike_get.sh
@@ -22,12 +22,6 @@ MAX_TIMEOUT=10
 
 . ./common_get
 
-#### MTB DOUBLE 29 ####
-MTB_DOUBLE_29_BASE="${URL}/bicicletas/rgidas/?p="
-MTB_DOUBLE_29_PAGES="1"
-
-bubic_get_page_outfile "${MTB_DOUBLE_29_BASE}" "${MTB_DOUBLE_29_PAGES}" mtb-double-29
-
 #### MTB RIGID ####
 MTB_RIGID_BASE="${URL}/bicicletas/rgidas/?p="
 MTB_RIGID_PAGES="$(seq 1 3)"
@@ -111,4 +105,85 @@ KIDS_BASE="${URL}/bicicletas/nio/?p="
 KIDS_PAGES="$(seq 1 4)"
 
 bubic_get_page_outfile "${KIDS_BASE}" "${KIDS_PAGES}" kids
+
+########################### SALE MODELS ###########################
+#### MTB RIGID ####
+MTB_RIGID_BASE="${URL}/ofertas/rgidas/?p="
+MTB_RIGID_PAGES="$(seq 1 5)"
+
+bubic_get_page_outfile "${MTB_RIGID_BASE}" "${MTB_RIGID_PAGES}" mtb-rigid-sale
+
+#### MTB DOUBLE ####
+MTB_DOUBLE_BASE="${URL}/ofertas/doble-suspensin/?p="
+MTB_DOUBLE_PAGES="$(seq 1 5)"
+
+bubic_get_page_outfile "${MTB_DOUBLE_BASE}" "${MTB_DOUBLE_PAGES}" mtb-double-sale
+
+#### MTB RIGID 29 ####
+MTB_RIGID_29_BASE="${URL}/ofertas/29er/?p="
+MTB_RIGID_29_PAGES="$(seq 1 5)"
+bubic_get_page_outfile "${MTB_RIGID_29_BASE}" "${MTB_RIGID_29_PAGES}" mtb-29-sale
+
+#### DIRT/BMX ####
+DIRT_BMX_BASE="${URL}/ofertas/dirt-bmx/?p="
+DIRT_BMX_PAGES="$(seq 1 2)"
+
+bubic_get_page_outfile "${DIRT_BMX_BASE}" "${DIRT_BMX_PAGES}" dirt-bmx-sale
+
+#### MTB WOMAN ####
+MTB_WOMAN_BASE="${URL}/ofertas/para-mujer/?p="
+MTB_WOMAN_PAGES="$(seq 1 5)"
+
+bubic_get_page_outfile "${MTB_WOMAN_BASE}" "${MTB_WOMAN_PAGES}" mtb-woman-sale
+
+#### URBAN CROSS ####
+URBAN_CROSS_BASE="${URL}/ofertas/cross/?p="
+URBAN_CROSS_PAGES="$(seq 1 5)"
+
+bubic_get_page_outfile "${URBAN_CROSS_BASE}" "${URBAN_CROSS_PAGES}" urban-cross-sale
+
+#### URBAN WALK ####
+URBAN_WALK_BASE="${URL}/ofertas/paseo/?p="
+URBAN_WALK_PAGES="$(seq 1 5)"
+
+bubic_get_page_outfile "${URBAN_WALK_BASE}" "${URBAN_WALK_PAGES}" urban-walk-sale
+
+#### URBAN FITNESS ####
+URBAN_FITNESS_BASE="${URL}/ofertas/paseo/?p="
+URBAN_FITNESS_PAGES="$(seq 1 2)"
+
+bubic_get_page_outfile "${URBAN_FITNESS_BASE}" "${URBAN_FITNESS_PAGES}" urban-fitness-sale
+
+#### ROAD ####
+ROAD_BASE="${URL}/ofertas/carretera/?p="
+ROAD_PAGES="$(seq 1 5)"
+
+bubic_get_page_outfile "${ROAD_BASE}" "${ROAD_PAGES}" road-sale
+
+#### ROAD_WOMAN ####
+ROAD_WOMAN_BASE="${URL}/ofertas/mujer/?p="
+ROAD_WOMAN_PAGES="$(seq 1 2)"
+
+bubic_get_page_outfile "${ROAD_WOMAN_BASE}" "${ROAD_WOMAN_PAGES}" road-woman-sale
+
+#### ROAD_TRIATLON ####
+ROAD_TRIATLON_BASE="${URL}/ofertas/triatln/?p="
+ROAD_TRIATLON_PAGES="$(seq 1 2)"
+
+bubic_get_page_outfile "${ROAD_TRIATLON_BASE}" "${ROAD_TRIATLON_PAGES}" road-triatlon-sale
+
+#### ROAD_CICLOCROSS ####
+ROAD_CICLOCROSS_BASE="${URL}/ofertas/ciclocross/?p="
+ROAD_CICLOCROSS_PAGES="$(seq 1 2)"
+
+bubic_get_page_outfile "${ROAD_CICLOCROSS_BASE}" "${ROAD_CICLOCROSS_PAGES}" road-ciclocross-sale
+
+#### KIDS ####
+KIDS_BASE="${URL}/ofertas/nio/?p="
+KIDS_PAGES="$(seq 1 5)"
+
+bubic_get_page_outfile "${KIDS_BASE}" "${KIDS_PAGES}" kids-sale
+
+
+
 
