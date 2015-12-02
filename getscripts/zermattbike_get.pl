4b2741c27008bb5c65251b7153583e1dde25663c
sarroutbi <sarroutbi@yahoo.es>
2015-12-02 21:48:02 +0100

    fix #147 Recovering Zermatt store

--- getscripts/zermattbike_get.pl
+++ getscripts/zermattbike_get.pl
@@ -21,6 +21,13 @@ use constant EXCLUDE => "-Rgif -Rpng -Rjpg";
 use constant MAX_TRIES => 10;
 use constant MAX_TIMEOUT => 10;
 
+#### ADVENTURE ####
+my $ADVENTURE_BASE = URL . "/categoria-producto/bicicletas/aventura/page/";
+my @ADVENTURE_PAGES = `seq 1 2`;
+my $ADVENTURE_FILE = "road-trekking";
+
+bubicpl_get_page($ADVENTURE_BASE, \@ADVENTURE_PAGES, $ADVENTURE_FILE, 10, 10, 1);
+
 #### BMX ####
 my $BMX_BASE = URL . "/categoria-producto/bicicletas/bmxcampillo/page/";
 my @BMX_PAGES = `seq 1 2`;
@@ -28,9 +35,18 @@ my $BMX_FILE = "bmx";
 
 bubicpl_get_page($BMX_BASE, \@BMX_PAGES, $BMX_FILE, 10, 10, 1);
 
+#### ROAD_TRIATLON ####
+my $ROAD_TRIATLON_BASE = URL . "/categoria-producto/bicicletas/triatlon-carretera/page/";
+my @ROAD_TRIATLON_PAGES = `seq 1 2`;
+my $ROAD_TRIATLON_FILE = "road-triatlon";
+
+chomp @ROAD_TRIATLON_PAGES;
+
+bubicpl_get_page($ROAD_TRIATLON_BASE, \@ROAD_TRIATLON_PAGES, $ROAD_TRIATLON_FILE, 10, 10, 1);
+
 #### ROAD ####
 my $ROAD_BASE = URL . "/categoria-producto/bicicletas/carretera/page/";
-my @ROAD_PAGES = `seq 1 2`;
+my @ROAD_PAGES = `seq 1 7`;
 my $ROAD_FILE = "road";
 
 chomp @ROAD_PAGES;
@@ -64,7 +80,7 @@ bubicpl_get_page($TREKKING_BASE, \@TREKKING_PAGES, $TREKKING_FILE, 10, 10, 1);
 
 #### MTB ####
 my $MTB_BASE = URL . "/categoria-producto/bicicletas/montana/page/";
-my @MTB_PAGES = `seq 1 2`;
+my @MTB_PAGES = `seq 1 7`;
 my $MTB_FILE = "mtb";
 
 chomp @MTB_PAGES;
@@ -79,6 +95,24 @@ my $FOLDING_FILE = "urban-folding";
 chomp @FOLDING_PAGES;
 
 bubicpl_get_page($FOLDING_BASE, \@FOLDING_PAGES, $FOLDING_FILE, 10, 10, 1);
+
+#### URBAN ####
+my $URBAN_BASE = URL . "/categoria-producto/bicicletas/urbana/page/";
+my @URBAN_PAGES = `seq 1 2`;
+my $URBAN_FILE = "urban";
+
+chomp @URBAN_PAGES;
+
+bubicpl_get_page($URBAN_BASE, \@URBAN_PAGES, $URBAN_FILE, 10, 10, 1);
+
+#### URBAN_WALK ####
+my $URBAN_WALK_BASE = URL . "/categoria-producto/bicicletas/multi-uso/page/";
+my @URBAN_WALK_PAGES = `seq 1 2`;
+my $URBAN_WALK_FILE = "urban-walk";
+
+chomp @URBAN_WALK_PAGES;
+
+bubicpl_get_page($URBAN_WALK_BASE, \@URBAN_WALK_PAGES, $URBAN_WALK_FILE, 10, 10, 1);
 
 #### MTB-DOWNHILL ####
 my $MTB_DOWNHILL_BASE = URL . "/categoria-producto/bicicletas/montana/descenso/page/";
