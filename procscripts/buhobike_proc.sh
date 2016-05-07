57a270507e14c21846f42fce889ef99b2aa2f58d
sarroutbi <sarroutbi@yahoo.es>
2016-05-07 17:17:45 +0200

    fix #154 Fixing BikeStocks store parsing

--- procscripts/buhobike_proc.sh
+++ procscripts/buhobike_proc.sh
@@ -136,7 +136,7 @@ function process_file()
     TRADEMARK=$(grep -A10 ">${MODEL}<" "${BASE_FILE}" | grep '<p class="pro_list_manufacturer">' | sed -e 's/<[^>]*>//g')
     TRADEMARK_CLEAN=${TRADEMARK}
     FINAL_URL=$(echo "${bike}" | awk -F 'href=' {'print $2'} | awk {'print $1'} | tr -d '"')
-    PRICE=$( grep -A10 ">${MODEL}<" "${BASE_FILE}" | grep '<span itemprop="price"' | sed -e 's/<[^>]*>//g' | egrep -E -o "[0-9]{0,2} {0,1}[0-9]{3},{1}[0-9]{0,2}" | tr -d ' ')
+    PRICE=$( grep -A10 ">${MODEL}<" "${BASE_FILE}" | grep '<span itemprop="price"' | sed -e 's/<[^>]*>//g' | egrep -E -o "[0-9]{0,2} {0,1}[0-9]{3},{1}[0-9]{0,2}" | tr -d ' ' | head -1)
     TRADEMARK_CAMEL=$(bubic_camel "${TRADEMARK_CLEAN}" 0)
     MODEL_CAMEL=$(bubic_camel "${MODEL_CLEAN}" ${NO_CAMEL_MIN})
     MODEL_CAMEL_DEF=$(echo "${MODEL_CAMEL}" | sed -e "s@${TRADEMARK_CAMEL}@@g")
