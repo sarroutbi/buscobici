92a3bcf2526e9a81067d889d64124a7cf2d26356
sarroutbi <sarroutbi@yahoo.es>
2016-05-07 18:50:39 +0200

    minor changes to enhance King Barcelona parsing

--- getscripts/kingbarcelona_get.sh
+++ getscripts/kingbarcelona_get.sh
@@ -25,55 +25,55 @@ MAX_TIMEOUT=10
 . ./common_get
 
 MTB_FIX_29_BIKES_BASE="${URL}/es/rigida-29-aluminio-c-644_646.html?page="
-MTB_FIX_29_BIKES_PAGES="$(seq 1 4)"
+MTB_FIX_29_BIKES_PAGES="$(seq 1 1)"
 
 MTB_FIX_29_CARBON_BIKES_BASE="${URL}/es/rigida-29-carbono-c-644_645.html?page="
-MTB_FIX_29_CARBON_BIKES_PAGES="$(seq 1 4)"
+MTB_FIX_29_CARBON_BIKES_PAGES="$(seq 1 2)"
 
 MTB_FIX_275_BIKES_BASE="${URL}/es/rigida-275-aluminio-c-644_648.html?page="
-MTB_FIX_275_BIKES_PAGES="$(seq 1 5)"
+MTB_FIX_275_BIKES_PAGES="$(seq 1 1)"
 
 MTB_FIX_275_CARBON_BIKES_BASE="${URL}/es/rigida-275-carbono-c-644_647.html?page="
-MTB_FIX_275_CARBON_BIKES_PAGES="$(seq 1 2)"
+MTB_FIX_275_CARBON_BIKES_PAGES="$(seq 1 1)"
 
 URBAN_ELECTRIC_BIKES_BASE="${URL}/es/ebikes-c-644_656.html?page="
-URBAN_ELECTRIC_BIKES_PAGES="$(seq 1 4)"
+URBAN_ELECTRIC_BIKES_PAGES="$(seq 1 1)"
 
 MTB_DOUBLE_29_BIKES_BASE="${URL}/es/doble-suspension-29-aluminio-c-644_650.html?page="
-MTB_DOUBLE_29_BIKES_PAGES="$(seq 1 2)"
+MTB_DOUBLE_29_BIKES_PAGES="$(seq 1 1)"
 
 MTB_DOUBLE_29_CARBON_BIKES_BASE="${URL}/es/doble-suspension-29-carbono-c-644_649.html?page="
-MTB_DOUBLE_29_CARBON_BIKES_PAGES="$(seq 1 2)"
+MTB_DOUBLE_29_CARBON_BIKES_PAGES="$(seq 1 1)"
 
 MTB_DOUBLE_275_BIKES_BASE="${URL}/es/doble-suspension-275-aluminio-c-644_652.html?page="
-MTB_DOUBLE_275_BIKES_PAGES="$(seq 1 3)"
+MTB_DOUBLE_275_BIKES_PAGES="$(seq 1 1)"
 
 MTB_DOUBLE_275_CARBON_BIKES_BASE="${URL}/es/doble-suspension-275-carbono-c-644_651.html?page="
-MTB_DOUBLE_275_CARBON_BIKES_PAGES="$(seq 1 3)"
+MTB_DOUBLE_275_CARBON_BIKES_PAGES="$(seq 1 1)"
 
 ROAD_BIKES_BASE="${URL}/es/carretera-c-644_653.html?page="
-ROAD_BIKES_PAGES="$(seq 1 3)"
+ROAD_BIKES_PAGES="$(seq 1 2)"
 
 ROAD_DISC_BIKES_BASE="${URL}/es/carretera-disco-c-644_654.html?page="
-ROAD_DISC_BIKES_PAGES="$(seq 1 2)"
+ROAD_DISC_BIKES_PAGES="$(seq 1 1)"
 
 ROAD_CICLOCROSS_BIKES_BASE="${URL}/es/ciclo-cross-c-644_658.html?page="
-ROAD_CICLOCROSS_BIKES_PAGES="$(seq 1 2)"
+ROAD_CICLOCROSS_BIKES_PAGES="$(seq 1 1)"
 
 URBAN_BIKES_BASE="${URL}/es/trekking-touring-c-644_673.html?page="
-URBAN_BIKES_PAGES="$(seq 1 6)"
+URBAN_BIKES_PAGES="$(seq 1 1)"
 
 URBAN_FOLDING_BIKES_BASE="${URL}/es/plegable-c-644_678.html?page="
-URBAN_FOLDING_BIKES_PAGES="$(seq 1 3)"
+URBAN_FOLDING_BIKES_PAGES="$(seq 1 1)"
 
 URBAN_FAT_BIKES_BASE="${URL}/es/fatbike-c-644_659.html?page="
-URBAN_FAT_BIKES_PAGES="$(seq 1 2)"
+URBAN_FAT_BIKES_PAGES="$(seq 1 1)"
 
 KIDS_BIKES_BASE="${URL}/es/bicicletas-infantiles-c-644_655.html?page="
-KIDS_BIKES_PAGES="$(seq 1 5)"
+KIDS_BIKES_PAGES="$(seq 1 1)"
 
-KIDS_BTT_BIKES_BASE="${URL}/es/bicicletas-26-c-644_675.html"
-KIDS_BTT_BIKES_PAGES=""
+KIDS_BTT_BIKES_BASE="${URL}/es/bicicletas-26-c-644_675.html?page="
+KIDS_BTT_BIKES_PAGES="$(seq 1 1)"
 
 bubic_get_page_outfile "${MTB_FIX_29_BIKES_BASE}" "${MTB_FIX_29_BIKES_PAGES}" mtb-fix-29
 bubic_get_page_outfile "${MTB_FIX_29_CARBON_BIKES_BASE}" "${MTB_FIX_29_CARBON_BIKES_PAGES}" mtb-fix-29-carbon
